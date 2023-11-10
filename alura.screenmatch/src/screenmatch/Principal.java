036b2b0327db76fdab8f60d613d6f77ba00998f6
andre abreu <aanddre@gmail.com>
2023-11-09 23:09:14 -0300

    02.12-Controlando Acesso

--- alura.screenmatch/src/screenmatch/Principal.java
+++ alura.screenmatch/src/screenmatch/Principal.java
@@ -1,14 +1,16 @@
 package screenmatch;
 
+import br.com.screenmatch.modelos.Filme;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
 		Filme meuFilme = new Filme(); // Tipo referência, objeto na memória, variável de referênciar
 
-		meuFilme.nome = "O Poderoso Chefão";
-		meuFilme.anoDeLancamento = 1970;
-		meuFilme.duracaoEmMinutos = 180;
+		meuFilme.setNome("O Poderoso Chefão");
+		meuFilme.setAnoDeLancamento(1970);
+		meuFilme.setDuracaoEmMinutos(180);
 
 		meuFilme.exibeFichaTecnica();
 
